a3672f1307ef2024152c2355fab58e6e923b9e4a
merliariza <180667185+merliariza@users.noreply.github.com>
2024-12-05 08:07:55 -0500

    :sparkles: js source

---
+++ js/app.js
@@ -0,0 +1,5 @@
+console.log('Saludos Campers desde JS');
+const usuario = "Ana";
+const saldo = 1500;
+const mensajeBienvenida = `Hola, ${usuario}! Tienes $${saldo} en tu cuenta.`;
+console.log(mensajeBienvenida);
